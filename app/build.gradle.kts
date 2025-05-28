a5ccf775b7f920d703f8bb17772d1f2ff7773b2b
ImGaram <gim78014@gmail.com>
2025-05-28 16:58:25 +0900

    :bug: :: 빌드 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.caramm.textextractor"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.caramm.textextractor"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

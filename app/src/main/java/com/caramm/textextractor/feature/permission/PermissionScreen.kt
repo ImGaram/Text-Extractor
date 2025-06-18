35863a97089c89337502e0b3db80babd07eb2dc2
ImGaram <gim78014@gmail.com>
2025-06-18 16:40:53 +0900

    :sparkles: :: permission 화면 하위 composable 위치 세팅

--- app/src/main/java/com/caramm/textextractor/feature/permission/PermissionScreen.kt
+++ app/src/main/java/com/caramm/textextractor/feature/permission/PermissionScreen.kt
@@ -1,13 +1,22 @@
 package com.caramm.textextractor.feature.permission
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PermissionScreen(navigateToPager: () -> Unit) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text("permission screen")
 
         Button(
@@ -16,4 +25,10 @@ fun PermissionScreen(navigateToPager: () -> Unit) {
             Text("navigate pager screen")
         }
     }
+}
+
+@Preview
+@Composable
+private fun PermissionScreenPreview() {
+    PermissionScreen {  }
 }

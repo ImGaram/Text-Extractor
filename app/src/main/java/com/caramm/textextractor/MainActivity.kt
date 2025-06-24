692ba2917cab8f3cf200120e2e14b541ca7691ab
ImGaram <gim78014@gmail.com>
2025-06-24 16:23:22 +0900

    :sparkles: :: navigation bar 안드로이드 버전에 따른 색상 적용

--- app/src/main/java/com/caramm/textextractor/MainActivity.kt
+++ app/src/main/java/com/caramm/textextractor/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.caramm.textextractor
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.rememberNavController
 import com.caramm.textextractor.common.navigation.TextExtractorNavigation
 import com.caramm.textextractor.ui.theme.TextExtractorTheme
@@ -22,14 +23,19 @@ class MainActivity : ComponentActivity() {
             TextExtractorTheme {
                 val navController = rememberNavController()
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    window.isNavigationBarContrastEnforced = false
+                } else {
+                    window.navigationBarColor = TextExtractorTheme.colors.background.toArgb()
+                }
+
                 TextExtractorNavigation(
                     navController = navController,
 //                    startDestination = ,
                     modifier = Modifier
                         .fillMaxSize()
                         .background(TextExtractorTheme.colors.background)
-                        .statusBarsPadding()
-                        .systemBarsPadding()
+                        .navigationBarsPadding()
                 )
             }
         }

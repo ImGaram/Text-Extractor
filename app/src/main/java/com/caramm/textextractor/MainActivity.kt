257b908db7a527341bb0f3d4d54cb17aefa66630
ImGaram <gim78014@gmail.com>
2025-06-25 16:48:19 +0900

    :sparkles: :: 권한 허용 상태에 따른 startDestination 변경 로직 추가

--- app/src/main/java/com/caramm/textextractor/MainActivity.kt
+++ app/src/main/java/com/caramm/textextractor/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.caramm.textextractor
 
+import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.rememberNavController
+import com.caramm.textextractor.common.navigation.Route
 import com.caramm.textextractor.common.navigation.TextExtractorNavigation
 import com.caramm.textextractor.ui.theme.TextExtractorTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +31,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             TextExtractorTheme {
                 val navController = rememberNavController()
+                val permissionState = rememberMultiplePermissionsState(
+                    permissions = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+                        listOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                        )
+                    } else emptyList()
+                )
+                val hasPermission = remember {
+                    mutableStateOf(
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) Environment.isExternalStorageManager()
+                        else permissionState.allPermissionsGranted
+                    )
+                }
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     window.isNavigationBarContrastEnforced = false
@@ -31,10 +54,11 @@ class MainActivity : ComponentActivity() {
 
                 TextExtractorNavigation(
                     navController = navController,
-//                    startDestination = ,
+                    startDestination = if (!hasPermission.value) Route.Permission else Route.Pager,
                     modifier = Modifier
                         .fillMaxSize()
                         .background(TextExtractorTheme.colors.background)
+                        .statusBarsPadding()
                         .navigationBarsPadding()
                 )
             }

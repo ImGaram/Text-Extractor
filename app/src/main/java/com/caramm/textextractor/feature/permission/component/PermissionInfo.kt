88748a8194fe512e0563fbb913225113a73c4540
ImGaram <gim78014@gmail.com>
2025-06-25 16:50:55 +0900

    :lipstick: :: 권한 정보 component 생성

---
+++ app/src/main/java/com/caramm/textextractor/feature/permission/component/PermissionInfo.kt
@@ -0,0 +1,75 @@
+package com.caramm.textextractor.feature.permission.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.caramm.textextractor.R
+import com.caramm.textextractor.core.designsystem.TextExtractorIcon
+import com.caramm.textextractor.core.designsystem.TextExtractorText
+import com.caramm.textextractor.ui.theme.TextExtractorTheme
+
+@Composable
+fun PermissionInfo(
+    modifier: Modifier,
+    iconVector: ImageVector,
+    iconTint: Color,
+    title: String,
+    description: String
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .background(TextExtractorTheme.colors.gray)
+                .padding(8.dp)
+        ) {
+            TextExtractorIcon(
+                imageVector = iconVector,
+                tint = iconTint
+            )
+        }
+
+        Column(modifier = Modifier.padding(top = 4.dp, start = 16.dp, bottom = 4.dp)) {
+            TextExtractorText(
+                text = title,
+                style = TextExtractorTheme.type.medium14
+            )
+
+            TextExtractorText(
+                text = description,
+                modifier = Modifier.padding(top = 4.dp),
+                style = TextExtractorTheme.type.normal12
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PermissionInfoPreview() {
+    PermissionInfo(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp),
+        iconVector = ImageVector.vectorResource(R.drawable.ic_folder),
+        iconTint = TextExtractorTheme.colors.text,
+        title = "저장소",
+        description = "기기 안에 파일 정보를 불러와 파일 생성 및 파일을 읽고, 쓰기 위해 저장소 권한이 필요합니다."
+    )
+}

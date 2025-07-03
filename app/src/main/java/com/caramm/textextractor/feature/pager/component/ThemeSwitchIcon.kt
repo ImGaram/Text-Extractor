e6ade5e80fb7485ec0a305f639651e501c55d500
ImGaram <gim78014@gmail.com>
2025-07-03 17:39:34 +0900

    :recycle: :: theme switch 버튼 viewModel 로직으로 교체

--- app/src/main/java/com/caramm/textextractor/feature/pager/component/ThemeSwitchIcon.kt
+++ app/src/main/java/com/caramm/textextractor/feature/pager/component/ThemeSwitchIcon.kt
@@ -1,22 +1,30 @@
 package com.caramm.textextractor.feature.pager.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.caramm.textextractor.R
+import com.caramm.textextractor.common.utils.Theme
 import com.caramm.textextractor.core.designsystem.TextExtractorIconButton
-import com.caramm.textextractor.core.persentation.ui.theme.TextExtractorTheme
+import com.caramm.textextractor.core.persentation.ThemeViewModel
 
 @Composable
-fun ThemeSwitchButton(modifier: Modifier = Modifier) {
+fun ThemeSwitchButton(
+    modifier: Modifier = Modifier,
+    themeViewModel: ThemeViewModel = hiltViewModel()
+) {
+    val theme = themeViewModel.theme.collectAsState()
+
     TextExtractorIconButton(
         onClick = {
-            TextExtractorTheme.updateTheme()
+            themeViewModel.updateTheme()
         },
         modifier = modifier,
         imageVector = ImageVector.vectorResource(
-            if (TextExtractorTheme.isDarkTheme()) R.drawable.ic_theme_dark
+            if (theme.value == Theme.DARK) R.drawable.ic_theme_dark
             else R.drawable.ic_light_theme
         )
     )

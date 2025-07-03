87d0b3002936b678d085501befc2e4c8b393fb33
ImGaram <gim78014@gmail.com>
2025-07-03 17:35:09 +0900

    :bug: :: ksp, kotlin 버전 충돌 오류 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     ksp(libs.androidx.hilt.complier)
-    implementation(libs.androidx.hilt.navigation)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
